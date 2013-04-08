e74a08c8433c20bb3dde25154cf8ad18c78301af
Benjamin Armintor <armintor@gmail.com>
2013-04-08 15:30:13 -0400

    poll returns null on timeout, of course

--- src/main/java/org/fcrepo/federation/bagit/ManifestMonitor.java
+++ src/main/java/org/fcrepo/federation/bagit/ManifestMonitor.java
@@ -8,13 +8,13 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ManifestMonitor implements Runnable {
-	
+
 	private BagItConnector connector;
-	
+
 	private BagItWatchService watchService;
-	
+
 	private volatile boolean shutdown;
-		
+
 	public ManifestMonitor(BagItConnector connector) throws IOException {
 		this.connector = connector;
 		this.watchService = new BagItWatchService(connector.getBagItDirectory());
@@ -25,32 +25,34 @@ public class ManifestMonitor implements Runnable {
 	public void run() {
 		while(!this.shutdown) {
 			try {
-			WatchKey key = watchService.poll(1, TimeUnit.SECONDS);
-			List<WatchEvent<?>> events = key.pollEvents();
-			boolean manifest = false;
-			boolean tagManifest = false;
-			Path path = null;
-			for (WatchEvent<?> event: events) {
-				Path context = (Path)event.context();
-				if (watchService.isManifest(context)) {
-					manifest = true;
-					path = context;
-				} else if (watchService.isTagManifest(context)) {
-					tagManifest = true;
-					path = context;
+				WatchKey key = watchService.poll(1, TimeUnit.SECONDS);
+				if (key != null) {
+					List<WatchEvent<?>> events = key.pollEvents();
+					boolean manifest = false;
+					boolean tagManifest = false;
+					Path path = null;
+					for (WatchEvent<?> event: events) {
+						Path context = (Path)event.context();
+						if (watchService.isManifest(context)) {
+							manifest = true;
+							path = context;
+						} else if (watchService.isTagManifest(context)) {
+							tagManifest = true;
+							path = context;
+						}
+					}
+					if (manifest) {
+						connector.changeManifest(path.toFile());
+					} else if (tagManifest) {
+						connector.changeTagFile(path.toFile());
+					}
 				}
-			}
-			if (manifest) {
-				connector.changeManifest(path.toFile());
-			} else if (tagManifest) {
-				connector.changeTagFile(path.toFile());
-			}
 			} catch (InterruptedException e) {
 				this.shutdown = true;
 			}
 		}
 	}
-	
+
 	public void shutdown() {
 		this.shutdown = true;
 	}
